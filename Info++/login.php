3101cc2b206b7c4f0641a487ab793b593eb27588
zoretibo <zoretibo@pc5660.TI-Lab-102.cegepsherbrooke.qc.ca.>
2017-08-21 11:03:47 -0400

    ok

--- Info++/login.php
+++ Info++/login.php
@@ -2,7 +2,6 @@
 include $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/database_connect.php';
 require_once $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/system/header.php';
 ?>
-    	<link rel="stylesheet" type="text/css" href="css/style.css">
     	<title>Connexion</title>
 	</head>
     <body>
@@ -12,8 +11,13 @@ require_once $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/system/header.ph
     </div>
     
         <form class="loginbox">
-    		
+    		 <label for='courriel'>Courriel</label><br><input name='courriel' class='' placeholder='Courriel'></input>
+    		 <br><label for='motPasse'>Mot de passe</label><br><input name='motPasse' class='' placeholder='Mot de passe'></input>
+    
+    		 <button class='buttonConnexion'></button>
     	</form>
+
+		 
   
     <?php 
         include $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/database_connect.php';
--- Info++/system/header.php
+++ Info++/system/header.php
@@ -6,9 +6,8 @@ echo "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
 
 /*echo " <link href='Css/dataTables.bootstrap4.css' rel='stylesheet'>";
 echo " <link href='Css/bootstrap.min.css' rel='stylesheet'>";
-echo " <link href='font-awesome/css/font-awesome.css' rel='stylesheet'>";
+echo " <link href='font-awesome/css/font-awesome.css' rel='stylesheet'>";*/
 
-echo " <link href='Css/sb-admin.min.css' rel='stylesheet'>";
-echo " <link href='Css/fastech_style.css' rel='stylesheet'>";
-echo "<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet'>";*/
+echo " <link href='css/style.css' rel='stylesheet'>";
+//echo "<link href='https://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet'>";
 ?>
