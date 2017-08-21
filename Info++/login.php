ce3c3e68d97bbdf15df2d4a301db50df88570195
zoretibo <zoretibo@pc5660.TI-Lab-102.cegepsherbrooke.qc.ca.>
2017-08-21 09:41:19 -0400

    ok

--- Info++/login.php
+++ Info++/login.php
@@ -1,4 +1,4 @@
 <?php 
-    include $_SERVER ["DOCUMENT_ROOT"] . '/Info++/database_connect.php';
+    //include $_SERVER ["DOCUMENT_ROOT"] . '/Info++/database_connect.php';
     echo "<br>allo</br>";
 ?>
