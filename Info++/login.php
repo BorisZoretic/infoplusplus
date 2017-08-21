6ff60ef9446fc1741d8988e4c14cf616c4446b81
zoretibo <zoretibo@pc5660.TI-Lab-102.cegepsherbrooke.qc.ca.>
2017-08-21 10:34:10 -0400

    ok

--- Info++/login.php
+++ Info++/login.php
@@ -1,4 +1,15 @@
 <?php 
     include $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/database_connect.php';
-    echo "<html><body><br>allo</br></body></html>";
+    require_once $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/system/header.php';
 ?>
+    	<link rel="stylesheet" type="text/css" href="Css/fastech_style_login.css">
+    	<title>Connexion</title>
+	</head>
+    <body>
+    	<br>allo</br>
+    <?php 
+        include $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/database_connect.php';
+        require_once $_SERVER ["DOCUMENT_ROOT"] . '/infoplusplus/Info++/system/footer.php';
+    ?>
+    </body>
+</html>
